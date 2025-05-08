b0664054cf10ac3398285dfde1e7b344b72969b4
jelkesudo <ficajelic@gmail.com>
2025-05-08 04:52:37 +0200

    header footer changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
+import { ShopPageComponent } from './pages/shop-page/shop-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'prodavnica', component: ShopPageComponent },
   { path: 'kontakt', component: ContactsPageComponent },
   { path: '**', component: LandingPageComponent }
 ];
