f40e06bf9e6c3d25a4a638ab8c7af8d6c8c8131f
jelkesudo <ficajelic@gmail.com>
2025-05-26 05:26:53 +0200

    testing promo banner on phone resolution

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
+import { PromoBannerComponent } from '../../components/promo-banner/promo-banner.component';
 
 @Component({
   selector: 'app-landing-page',
-  standalone: true,
-  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
